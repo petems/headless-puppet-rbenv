2cc50424734370fe918444f1476169adb3d60c3b
petems <p.morsou@gmail.com>
2013-08-28 15:53:37 +0100

    Changed command to use submodules

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -9,4 +9,6 @@ cd /etc/puppet
 git init
 git remote add origin https://github.com/petems/headless-puppet
 git pull -u origin master
+git submodule init
+git submodule update
 puppet apply /etc/puppet/manifests/default.pp --modulepath /etc/puppet/modules
