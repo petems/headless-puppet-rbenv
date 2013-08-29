a9ad501d0fcae7c917299c1992223b43e09ae9e0
petems <p.morsou@gmail.com>
2013-08-29 12:33:06 +0100

    Tweaks to vagrant provision script...

--- scripts/vagrant.sh
+++ scripts/vagrant.sh
@@ -8,7 +8,11 @@ apt-get install git puppet -y
 cd /etc/puppet
 if [ -d .git ]; then
   echo "Repo already setup, pulling changes!"
+  git pull master
+  git submodule init
+  git submodule update
 else
+  echo "Repo not setup, initialising!"
   git init
   git remote add origin https://github.com/petems/headless-puppet-rbenv
   git pull -u origin master
